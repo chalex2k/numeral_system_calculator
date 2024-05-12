dbe786e69869473cdb5d046d63d2b3454acc8498
alex <chalex2k@gmail.com>
2024-05-12 14:06:26 +0300

    Код 🟢 Выбор итоговой системы счисления

--- app/index.js
+++ app/index.js
@@ -12,3 +12,18 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const radioButtons = document.querySelectorAll('input[name="result_system"]');
+    const customInput = document.getElementById('customResultInput');
+
+    radioButtons.forEach(function(radioButton) {
+        radioButton.addEventListener('change', function() {
+            if (this.value === 'custom') {
+                customInput.style.display = 'block';
+            } else {
+                customInput.style.display = 'none';
+            }
+        });
+    });
+});
